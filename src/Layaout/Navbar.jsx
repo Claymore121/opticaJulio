4080f26724855878cad320a46f9db6df13394431
Julio Cesar Cazarez Uriarte <juliocazarez@MacBook-Pro-de-Julio.local>
2026-07-12 17:14:43 -0700

    Se avanzo en el Navbar

---
+++ src/Layaout/Navbar.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const navLinks = [
+    {label: "Inicio",href:"#inicio"},
+    {label: "Catalogo",href:"#catalogo"},
+    {label: "Contacto",href:"#contacto"},
+
+
+]
+
+
+export default function Navbar () {
+
+    return (
+        <>
+        
+        </>
+    )
+}
